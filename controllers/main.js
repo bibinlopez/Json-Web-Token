8cb3b379d8979cb71671d8ea176cc12ac424de71
John <bibinlopez36@gmail.com>
2023-05-30 18:54:54 +0530

    token verify

--- controllers/main.js
+++ controllers/main.js
@@ -7,8 +7,30 @@ const login = async (req, res) => {
    return res.status(200).json({ msg: 'user created', token })
 }
 
+const dashboard = async (req, res) => {
+   const authHeader = req.headers.authorization
+   // console.log(authHeader);
+   if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      throw new CustomAPIError('No token provided', 400)
+   }
+   const token = authHeader.split(' ')[1]
+   console.log(token);
+   try {
+      const decoded = jwt.verify(token, 'jwtsecret')
+      // console.log(decoded);
+      const { username } = decoded
+      req.user = username
+   } catch (error) {
+      throw new CustomAPIError('invalid token', 401)
+   }
 
+   // const  username= req.user
+   // console.log(username);
+   const luckyNumber = Math.floor(10 + Math.random() * 90)
+   res.status(200).json({ msg: `Hi ${req.user} your lucky number is ${luckyNumber}` })
+}
 
 module.exports = {
-   login
+   login,
+   dashboard
 }
--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const router = express.Router();
-const { login } = require('../controllers/main')
-
+const { login, dashboard } = require('../controllers/main')
 
 router.post('/login', login);
-
+router.get('/dashboard', dashboard);
 
 
 
